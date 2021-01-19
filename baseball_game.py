3d5058c4b8480faa5c8645dff2f14753b2a6c715
HJ_Kim <dr_moms@naver.com>
2021-01-19 12:59:11 +0900

    Update my code

--- baseball_game.py
+++ baseball_game.py
@@ -286,7 +286,33 @@ def main():
     print("Random Number is : ", random_number)
     # ===Modify codes below=============
     # 위의 코드를 포함하여 자유로운 수정이 가능함
+    while True:
+        check_result = 0
+        user_input = input('Input guess number : ')
+        if user_input == '0':
+            break
+        if is_validated_number(user_input):
+            result = get_strikes_or_ball(user_input, random_number)
+            print("Strikes : {} , Balls : {}".format(result[0], result[1]))
+            if result[0] == 3:
+                while True:
+                    print("You win, one more(Y/N) ?")
+                    check_continue = input()
+                    if is_no(check_continue):
+                        check_result = 1
+                        break
+                    if is_yes(check_continue):
+                        break
+                    else:
+                        print("Wrong Input, Input again")
+                if check_result == 1:
+                    break
+                else:
+                    random_number = str(get_not_duplicated_three_digit_number())
+                    print("Random Number is : ", random_number)
 
+        else:
+            print("Wrong Input, Input again")
 
     # ==================================
     print("Thank you for using this program")
